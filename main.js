41c6c782b5aeada11afaa274aa5e754e8b12dcd6
guilherme-dellarosa <144817922+guilherme-dellarosa@users.noreply.github.com>
2025-06-06 12:31:13 -0300

    .

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
+    for (let i = 10; i < contadores.length; i++) {
         document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
